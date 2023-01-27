57c003c643959fac1bef290aed6f297964ce636f
SUSOBHANLAL <susobhanlal2017@gmail.com>
2023-01-27 22:04:31 +0530

    first commit

--- src/decimal_to_binary/susobhan.java
+++ src/decimal_to_binary/susobhan.java
@@ -2,15 +2,25 @@ package decimal_to_binary;
 
 public class susobhan {
 	public static void main(String args[]) {
-		int num = 7;
+		
+		
+		
+		int num = 15;
+		
+		int sum = 0;
 		int temp = num;
 		int rem = 0;
-		while (temp!=0) {
-			 rem = temp %2;
-			System.out.print(" "+rem);
-			temp= temp/2;
+		int p =1;
+		while(temp!=0) {
+			rem = temp%2;
+			sum = sum+rem*p;
+			temp = temp/2;
+			p=p*10;
 			
 		}
+		
+		System.out.print(sum);
+		
 	}
 
 }
